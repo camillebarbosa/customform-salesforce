2f6ecc5d3df9f98127a79114d305b04a3010ffbc
Camille Barbosa dos Santos <camille.santos@desktop.tec.br>
2026-02-05 11:52:32 -0300

    Update new desing to lwc customform

--- lwc/customForm/customForm.js
+++ lwc/customForm/customForm.js
@@ -6,46 +6,66 @@ import { CloseActionScreenEvent } from 'lightning/actions';
 export default class CustomForm extends LightningElement {
     @api recordId; // Id do Case
     @track customerPriority;
+    @track showForm = true;
+    @track showResult = false;
+    @track isSuccess = false;
+
+
+
+    priorityOptions = [
+        { label: 'Alta', value: 'High' },
+        { label: 'Média', value: 'Medium' },
+        { label: 'Baixa', value: 'Low' }
+    ];
+
+handleChange(event) {
+    this.customerPriority = event.detail.value;
+}
+
 
     handleChange(event) {
         this.customerPriority = event.target.value;
     }
 
     handleSave() {
-        if (!this.recordId) {
-            console.error('recordId não definido');
-            return;
-        }
-
-        saveRecord({
-            record: {
-                Id: this.recordId,
-                Priority: this.customerPriority
-            }
-        })
-        .then(() => {
-            // Mensagem de sucesso
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: 'Sucesso',
-                    message: 'Customer Priority atualizado com sucesso!',
-                    variant: 'success'
-                })
-            );
-
-            // Fecha a aba/popup se estiver em quick action/modal
-            this.dispatchEvent(new CloseActionScreenEvent());
-        })
-        .catch(error => {
-            // Mensagem de erro
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: 'Erro ao salvar',
-                    message: error.body ? error.body.message : error.message,
-                    variant: 'error'
-                })
-            );
-            console.error(error);
-        });
+    if (!this.recordId) {
+        console.error('recordId não definido');
+        return;
     }
+
+    saveRecord({
+        record: {
+            Id: this.recordId,
+            Priority: this.customerPriority
+        }
+    })
+    .then(() => {
+        this.isSuccess = true;
+        this.showForm = false;
+        this.showResult = true;
+
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Sucesso',
+                message: 'Customer Priority atualizado com sucesso!',
+                variant: 'success'
+            })
+        );
+    })
+    .catch(error => {
+        this.isSuccess = false;
+        this.showForm = false;
+        this.showResult = true;
+
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Erro ao salvar',
+                message: error.body ? error.body.message : error.message,
+                variant: 'error'
+            })
+        );
+        console.error(error);
+    });
+}
+
 }
